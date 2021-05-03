155912148afea879c1d2906ef6dde69379169d95
rohilla3377 <76719862+rohilla3377@users.noreply.github.com>
2021-05-03 21:06:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,37 +2,47 @@
 
 
 
+var wall,car;
+var speed,weight;
 
-var fixedRect,movingrect,obj1;
+
+
+
+
 function setup() {
-  createCanvas(800,400);
- fixedRect= createSprite(400, 200, 50, 50);
- movingRect= createSprite(340,300,40,40);
- obj1=createSprite(200,240,60,50);
- obj1.shapeColor= "blue";
-fixedRect.shapeColor="green";
- movingRect.shapeColor="green";
+  createCanvas(1600,800);
+  wall=createSprite(1500, 200, 60, height);
+ car=createSprite(200,50,50,50);
+ 
+  wall.shapecolor=color(80,80,80);
+  car.shapeColor=color(255);
+  
+speed=random(55,90);
+weight=random(400,1500);
+car.velocityX = speed;
 }
 
 function draw() {
   background(255,255,255);  
-  movingRect.x =World.mouseX;
-  movingRect.y =World.mouseY;
- if(isTouching(movingRect,obj1)){
-   movingRect.shapeColor="orange";
-   obj1.shapeColor="black";
-   
-  
- }
- else{movingRect.shapeColor="yellow";obj1.shapeColor="yellow";}
- 
+  if(wall.x-car.x<(car.width+wall.width)/2)
+  {
+    car.velocityX=0;
+    var deformation=0.5 * weight * speed* speed/22509;
+    if(deformation>180)
+    {
+      car.shapeColor=color(255,0,0)
+
+    }
+        
+     if(deformation<180 && deformation>100)
+     {
+          car.shapeColor=color(230,230,0);
+     }
+
+     if(deformation<100)
+     {
+       car.shapeColor=color(0,255,0);
+     }
+    }
   drawSprites();
-}
-  function isTouching(object1,object2) {
-    if( (object1.x-object2.x < object2.width/2 + object1.width/2) &&
-    (object2.x-object1.x < object2.width/2 + object1.width/2) &&
-    (object1.y-object2.y < object2.height/2 + object1.height/2) &&
-    (object2.y-object1.y < object2.height/2 + object1.height/2) ) 
-    { return true  }
-    else{return false }
-  }
+  } 
